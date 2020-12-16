c0e568d3e6fc4c7f5bd7e6019e85d0e8a6e88005
nemo D'acremont <nemo@nemo.dacremont@gmail.com>
2020-12-16 07:26:52 +0000

    Added very usefull comments here and there

--- server/src/api/animes/getURL/extractURL.ts
+++ server/src/api/animes/getURL/extractURL.ts
@@ -3,6 +3,6 @@ import { Anime } from "../../../stores/animes";
 
 
 export default async function (anime: Anime): Promise<string> {
-
+	// TO-DO: actually everything
 	return '';
 }
--- server/src/stores/jsdom/index.ts
+++ server/src/stores/jsdom/index.ts
@@ -1,6 +1,7 @@
 
 import jsdom from 'jsdom';
 
+// TO-DO: Maybe at least read the documentation
 const pages: jsdom.JSDOM[] = [];
 
 export default pages;
