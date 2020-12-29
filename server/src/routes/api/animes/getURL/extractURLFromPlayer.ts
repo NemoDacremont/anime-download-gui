bfea4e3053f7fe3da701557ddfea3bc4ef95857d
nemo D'acremont <nemo@nemo.dacremont@gmail.com>
2020-12-29 13:06:42 +0000

    gonna use another parser, this one doesn't support typescript

--- server/src/routes/api/animes/getURL/extractURLFromPlayer.ts
+++ server/src/routes/api/animes/getURL/extractURLFromPlayer.ts
@@ -2,42 +2,65 @@
 //import axios from 'axios';
 import { browser, pages } from '../../../../stores/puppeteer';
 
-export default async function (playerURL: string): Promise<string | null> {
-	//const playerPage = (await axios.get(playerURL)).data;
+export default function (playerURL: string): Promise<string | null> {
+	return new Promise(async (resolve, reject) => {
+		//const playerPage = (await axios.get(playerURL)).data;
 
-	const pageIndex = pages.push(await browser.newPage());
-	const page = pages[pageIndex-1];
+		const pageIndex = pages.push(await browser.newPage());
+		const page = pages[pageIndex-1];
 
-	await page.goto(playerURL);
+		/*
+		// wanted to reject non same-origin request but it interfere
+		// with mystream support, keep it here if I finally find a
+		// way to remove trackers
+		page.on('request', (request) => {
+			const url = new URL(page.url());
+			const sameOriginReGex = new RegExp('^', url.origin);
+			console.log(sameOriginReGex);
 
-	/*
-	*		Scraping video's src attribute
-	*		if it exist, return. else try source tag
-	*/
-	const videoSrc = await page.$eval('video', (el) => el.getAttribute('src'));
-	if (typeof videoSrc === 'string' && videoSrc) {
-		await page.close();
-		return videoSrc;
-	}
+			if (sameOriginReGex.test(request.url())) {
+				request.continue();
+			}
+			else request.continue();
+		});*/
 
-	/*
-	*		Scraping source tag src attribute
-	*		if exist, return. else blob
-	*/
+		await page.goto(playerURL);
 
-	const sourceSrc = await page.$eval('source', (el) => el.getAttribute('src'));
-	if (sourceSrc) {
-		await page.close();
-		return sourceSrc;
-	}
+		/*
+		*		Scraping video's src attribute
+		*		if it exist, return. else try source tag
+		*/
+		const videoSrc = await page.$eval('video', (el) => el.getAttribute('src'));
+		if (videoSrc && typeof videoSrc === 'string') {
+			page.close();
+			resolve(videoSrc);
+		}
 
-	/*
-	*		Blob scraping
-	*/
+		/*
+		*		Scraping source tag src attribute
+		*		if exist, return. else blob
+		*/
 
-	// maybe latter, this means we can't get p-streaming file source.
-	
+		const sourceSrc = await page.$eval('source', (el) => el.getAttribute('src'));
+		if (sourceSrc) {
+			page.close();
+			resolve(sourceSrc);
+		}
 
-	await page.close();
-	return sourceSrc;
+		/*
+		*		Blob scraping
+		*/
+
+		page.on('response', async (response) => {
+			// This will only support pstream site for and for security
+			const manifestRegex = /^https:\/\/www\.pstream\.net\/\w\/\w*?\.m3u8/i;
+			if (manifestRegex.test(response.url())) {
+				console.log(await response.text());
+				page.close();
+				resolve(null);
+			}
+		});
+
+		// maybe latter, this means we can't get p-streaming file source.
+	});
 }
