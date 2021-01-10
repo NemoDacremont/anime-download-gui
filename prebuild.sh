d7fc6515989fece68affb7047219ecc0786f0cca
nemo D'acremont <nemo@nemo.dacremont@gmail.com>
2021-01-10 14:44:31 +0000

    The prod.sh file is now pretty satisfying

--- prebuild.sh
+++ prebuild.sh
@@ -1,7 +1,8 @@
 
+# Actually, this isn't necessary, I should build in temporary dir instead of deleting
+# previous build and the mv it
 rm -rf dist
 
+# Build the client
 cd client/
 npm run build
-
-
--- prod.sh
+++ prod.sh
@@ -1,31 +1,53 @@
 
-#npm run build
+## Functions
+function CREATE_NEW_DOT_ENV {
+	VARIABLES=(PORT=8080 NODE_ENV=production)
+	for VAR in ${VARIABLES[*]}
+	do
+		echo $VAR >> .env
+	done
+}
 
-mkdir prod
-
-FILES=(.env package.json package-lock.json LICENSE.md README.md)
+## Variables
+OUTDIR=prod
+OUTZIP=prod.zip
+FILES=(package.json package-lock.json LICENSE.md README.md)
 DIRECTORIES=(dist)
 
+## Script
+npm run build
+
+# Creating temporary a directory to build the production app
+mkdir $OUTDIR
+
+# Copy files
 for FILE in ${FILES[*]}
 do
-	cp FILE prod/
+	cp $FILE $OUTDIR
 done
 
+# Copy directories
 for DIR in ${DIRECTORIES[*]}
 do
-	cp -r DIR prod/
+	cp -r $DIR $OUTDIR
 done
 
-cd prod
+# Install production dependencies, so it can be runned without npm install when unzipped
+cd $OUTDIR
+# Create a brand new .env
+CREATE_NEW_DOT_ENV
+# Install prod dependencies
 npm install --only=prod
+
 
 # This file shouldn't be usefull in production mode, only scripts in package.json are
 rm package-lock.json
+# Remove .map files from the builded client
+rm dist/client/js/*.map
 
-
+# Then create the zip file and delete the old one and the directory that used to build the app
 cd ..
-zip prod.zip -d prod
-tree prod
+zip "${OUTZIP}.temp" -r $OUTDIR
+mv "${OUTZIP}.temp" $OUTZIP
 
-rm -r prod
-
+rm -r $OUTDIR
