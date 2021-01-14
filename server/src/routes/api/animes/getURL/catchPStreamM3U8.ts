abaa11eeae62659f50766759ece344b3622bf266
nemo D'acremont <nemo@nemo.dacremont@gmail.com>
2021-01-14 07:02:42 +0000

    Forgot to add files

---
+++ server/src/routes/api/animes/getURL/catchPStreamM3U8.ts
@@ -0,0 +1,17 @@
+
+import { Response, Page } from 'puppeteer-core';
+
+const pStreamManifestRegex = /^https:\/\/www\.pstream\.net\/\w\/\w*?\.m3u8/i;
+const manifestsRegex = /^https:\/\/www\.pstream\.net\/h\/\d+\/\w*?\.m3u8\?expires=\d+&signature=\w+/m;
+
+
+export default async function (response: Response): Promise<string | null> {
+	if (!pStreamManifestRegex.test(response.url())) return null;
+
+	const manifests = await response.text();
+
+	const bestManifests = manifests.match(manifestsRegex)
+	const bestManifestURL = bestManifests ? bestManifests[0]: null;
+
+	return bestManifestURL;
+}
