21028b34af37c970b7b26af8d677a91201c1daf9
Nemo D'acremont <nemo.dacremont@gmail.com>
2021-04-04 16:46:27 +0200

    removed puppeteer init from init script

--- server/src/utils/init/index.ts
+++ server/src/utils/init/index.ts
@@ -3,18 +3,11 @@
 import http from 'http';
 
 import cacheAnimeList from './cacheAnimeList';
-import { initPuppeteer } from '../../stores/puppeteer';
 import initSocketIOInstance from './initSocketIOInstance';
 import initCron, { updateNewsCron } from '../cron';
 
 async function initScript (httpServer: http.Server): Promise<void> {
 	await cacheAnimeList();
-	await (initPuppeteer()
-		.catch((err: Error | null) => {
-			console.log('An error occurred, this is probably due to the path for chromium in the config.json file')
-			console.log(err?.message);
-		}));
-
 	updateNewsCron();
 
 	initSocketIOInstance(httpServer);
