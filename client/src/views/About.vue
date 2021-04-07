beb130b370a2fd1080d881a779f9feb223731969
Nemo D'acremont <nemo.dacremont@gmail.com>
2021-04-07 11:18:48 +0200

    Added content to about page with some dirty style

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,46 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+		<div class="headers">
+			<h1>About</h1>
+		</div>
+		<div class="content">
+			<h2>Here you will find credits for technologies and things used in this project</h2>
+			<ul>
+
+				<li>Backend: Node.js with express.js</li>
+				<li>Frontend: Vue.js</li>
+				<li>Both use socketio</li>
+				<li>I'm using ffmpeg to download files like m3u8 playlists</li>
+				<li>Animes are downloaded from neko-sama.fr streaming website</li>
+
+			</ul>
+		</div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.headers {
+	height: 8vh;
+	padding: 0 5em;
+	display: flex;
+	background-color: var(--nav-background-color);
+	align-items: center;
+}
+
+.content {
+	padding: 1em;
+	display: flex;
+	flex-direction: column;
+	gap: .5em;
+	justify-content: center;
+	align-items: center;
+}
+
+ul {
+	width: 70%;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+}
+
+</style>
