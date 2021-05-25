a507c02d4445062b8d63b9807b945506f2be2e20
Nemo D'acremont <nemo.dacremont@gmail.com>
2021-05-25 12:11:53 +0200

    Added some useless comments and settings router

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,13 +1,17 @@
 
 import { Router } from 'express';
+
+//	API Routers
 import animeRouter from './animes';
 import downloadRouter from './download';
+import settingsRouter from './settings';
 
+//	API Router
 const api = Router();
 
 api.use('/animes/',  animeRouter);
-
 api.use('/download/', downloadRouter);
+api.use("/settings/", settingsRouter);
 
 api.all('*', (req, res) => {
 	res.status(400).send('400 - Bad Request');
