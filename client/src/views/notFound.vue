beed995d52a99d85d78bd9e2697a5017e6306443
Nemo D'acremont <nemo.dacremont@gmail.com>
2021-05-25 12:08:45 +0200

    notFound page minor update

--- client/src/views/notFound.vue
+++ client/src/views/notFound.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="404">
+	<div class="not-found">
 		<h1>404 - Not Found</h1>
 	</div>
 </template>
@@ -9,9 +9,22 @@ import { defineComponent } from 'vue'
 export default defineComponent({
 	name: '404',
 
-})
+});
 </script>
 
 <style lang="scss" scoped>
+
+div.not-found {
+	height: 100%;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	h1 {
+		position: relative;
+		transform: translateY(-5em);
+	}
+}
 
 </style>
