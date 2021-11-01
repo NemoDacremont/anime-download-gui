91c53b0911ae4a2986f9dae6799d66d3cafe091f
Nemo D'acremont <nemo.dacremont@gmail.com>
2021-11-01 12:35:43 +0100

    I have no idea what im doin

--- client/src/shims-vue.d.ts
+++ client/src/shims-vue.d.ts
@@ -1,6 +1,14 @@
 
+//declare module '*.vue' {
+  //import { DefineComponent } from 'vue';
+  //const component: DefineComponent;
+  //export default component;
+//}
+
+
+/* eslint-disable */
 declare module '*.vue' {
-  import { DefineComponent } from 'vue';
-  const component: DefineComponent;
-  export default component;
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
